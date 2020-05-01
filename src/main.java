08cee90d76c2254dbb7c40bc0a1397099cc8b3a1
Alan <48175897+alanptrieu@users.noreply.github.com>
2020-04-30 23:19:41 -0700

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,26 @@
+//Alan Trieu
+//CS2450
+//Assignment 4
+//May 4, 2020
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class main extends Application  {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Prototype");
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
